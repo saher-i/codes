cebb4fd6f7fe248be547b83116aeb3d565a22afe
Saher Iqbal <si2443@columbia.edu>
2024-03-23 14:07:20 -0400

    CUDA HW Part 1

--- CUDA1/Part-A/vecaddKernel01.cu
+++ CUDA1/Part-A/vecaddKernel01.cu
@@ -11,12 +11,12 @@
 /// without using coalesced memory access.
 /// 
 
-__global__ void AddVectors(const float* A, const float* B, float* C, int number_of_elements)
+__global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x; // Unique grid index of a thread
     int stride = blockDim.x * gridDim.x; // Total number of threads in the grid
 
-    for (int i = idx; i < number_of_elements; i += stride) {
+    for (int i = idx; i < N; i += stride) {
         C[i] = A[i] + B[i];
     }
 }

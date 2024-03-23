95339d49bb31c8aff41e71f67854e4624e00aa4a
Saher Iqbal <si2443@columbia.edu>
2024-03-23 15:15:11 -0400

    CUDA HW Part 1

--- CUDA1/Part-A/vecadd.cu
+++ CUDA1/Part-A/vecadd.cu
@@ -97,7 +97,7 @@ int main(int argc, char** argv)
 
     // Warm up
     AddVectors<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, ValuesPerThread);
-    AddVectorsCoalesced<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, ValuesPerThread);
+//    AddVectorsCoalesced<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, ValuesPerThread);
     error = cudaGetLastError();
     if (error != cudaSuccess) Cleanup(false);
     cudaDeviceSynchronize();
@@ -108,7 +108,7 @@ int main(int argc, char** argv)
 
     // Invoke kernel
     AddVectors<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, ValuesPerThread);
-    AddVectorsCoalesced<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, ValuesPerThread);
+  //  AddVectorsCoalesced<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, ValuesPerThread);
     error = cudaGetLastError();
     if (error != cudaSuccess) Cleanup(false);
     cudaDeviceSynchronize();

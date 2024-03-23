9adc1ab0f4c0fc893022f9a078b6e460aaf5f477
Saher Iqbal <si2443@columbia.edu>
2024-03-23 18:11:28 -0400

    try

--- CUDA1/Part-A/vecaddKernel01.cu
+++ CUDA1/Part-A/vecaddKernel01.cu
@@ -14,7 +14,7 @@
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x; // Unique grid index of a thread
-    if(idx < N) {
+    if(idx <= N) {
         C[idx] = A[idx] + B[idx];
     }
 }

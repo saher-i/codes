f749ac4fd5ff160fb8f620791e9129195c541e25
Saher Iqbal <si2443@columbia.edu>
2024-03-23 13:28:58 -0400

    CUDA HW Part 1

---
+++ CUDA1/Part-A/vecaddKernel01.cu
@@ -0,0 +1,23 @@
+///
+/// vecAddKernel00.cu
+/// For COMS E6998 Spring 2023
+/// Instructor: Parajit Dube and Kaoutar El Maghraoui
+/// Based on code from the CUDA Programming Guide
+/// By David Newman
+/// Created: 2011-02-16
+/// Last Modified: 2011-02-16 DVN
+///
+/// This Kernel adds two Vectors A and B in C on GPU
+/// without using coalesced memory access.
+/// 
+
+__global__ void AddVectorsCoalesced(const float* A, const float* B, float* C, int number_of_elements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x; // Unique grid index of a thread
+    int stride = blockDim.x * gridDim.x; // Total number of threads in the grid
+
+    for (int i = idx; i < number_of_elements; i += stride) {
+        C[i] = A[i] + B[i];
+    }
+}
+

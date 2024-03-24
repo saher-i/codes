da76f4ceefa58de003ea057e9ffdb047c85c2523
Saher Iqbal <si2443@columbia.edu>
2024-03-24 14:04:43 -0400

    modified files

--- CUDA1/Part-A/vecaddKernel01.cu
+++ CUDA1/Part-A/vecaddKernel01.cu
@@ -16,7 +16,12 @@ __global__ void AddVectors(const float *A, const float *B, float *C, int N) {
     // Global thread index calculation
     int index = blockIdx.x * blockDim.x + threadIdx.x;
 
+    for(int i = index; i < N; i = i+blockDim.x) {
+        C[i] = A[i] + B[i];
+    }
+/*
     for(int i = index; i < blockDim.x*gridDim.x*N; i = i+N) {
         C[i] = A[i] + B[i];
     }
+*/
 }

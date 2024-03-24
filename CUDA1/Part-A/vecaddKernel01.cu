af6e72f51f270975afce8ca21443d0f45ba057d8
Saher Iqbal <si2443@columbia.edu>
2024-03-24 11:26:39 -0400

    modified files

--- CUDA1/Part-A/vecaddKernel01.cu
+++ CUDA1/Part-A/vecaddKernel01.cu
@@ -12,7 +12,7 @@
 ///
 
 __global__ void AddVectors(const float *A, const float *B, float *C, int N) {
-    int idx = (blockDim.x * blockIdx.x + threadIdx.x)%N;
+    int idx = (blockDim.x * blockIdx.x + threadIdx.x);
     if (idx < N) {
             C[idx] = A[idx] + B[idx];
     }
